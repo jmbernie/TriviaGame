de1feaf724bc96ef265e904cfa03ae979bf1e7b1
Jesse Bernier <jesse_bernier@hotmail.com>
2017-09-30 00:43:37 -0400

    testing new responses in js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,34 +2,42 @@
 
 
 
+//correct answers
+var cat1 = ($("input[name=Question1]:checked").val() = "BIG");
+var cat2 = ($("input[name=Question2]:checked").val() = "tupac");
+var cat3 = ($("input[name=Question3]:checked").val() = "hill");
+var cat4 = ($("input[name=Question4]:checked").val() = "DeathRow");
+var cat5 = ($("input[name=Question5]:checked").val() = "quest");
+var cat6 = ($("input[name=Question6]:checked").val() = "bag");
+var cat7 = ($("input[name=Question7]:checked").val() = "jeff");
+var cat8 = ($("input[name=Question8]:checked").val() = "PublicEnemy");
 
-var cat1 = ($("input[name=Question1]:checked").val() != "BIG");
-var cat2 = ($("input[name=Question2]:checked").val() != "tupac");
-var cat3 = ($("input[name=Question3]:checked").val() != "hill");
-var cat4 = ($("input[name=Question4]:checked").val() != "DeathRow");
-var cat5 = ($("input[name=Question5]:checked").val() != "quest");
-var cat6 = ($("input[name=Question6]:checked").val() != "bag");
-var cat7 = ($("input[name=Question7]:checked").val() != "jeff");
-var cat8 = ($("input[name=Question8]:checked").val() != "PublicEnemy");
+var answersArray = [cat1,cat2,cat3,cat4,cat5,cat6,cat7,cat8];
+	// this is to test the vars set up
+	// for (var i=0; i < answersArray.length; i++){
+	// 	console.log(answersArray[i]);
+	// }
+
 
 // need to define and start a timer $('#timer-div') 
+	
+
 
 $('#restart').on('click', function(){
 		$('#results-div').empty();
 });
 
-$('#submit').on('click', function() {
+$('#submit').on('click', function() {//this never seems to trigger
+	alert("submit got clicked");
+	console.log("submit got clicked");
+	// lets just start by outputting the vars
+	// for (var i=0; i < answersArray.length; i++){
+	// 	console.log(answersArray[i]);
+	// }
 			// need to check for correct answers and display to $('#results-div')
-
-
-
 			// term = $('#searchTerm').val().trim();
-			// inputBegin = $('#start').val().trim();
-			// inputEnd = $('#end').val().trim();
-			// number = $('input[name=radioName]:checked', '#myForm').val();
-
-			// console.log($('input[name=radioName]:checked', '#myForm').val())
-
+			// number = $('input[name=Question8]:checked', '#myForm').val();
+			// console.log($('input[name=Question8]:checked', '#myForm').val())
 			// displayResults();
 });
 
